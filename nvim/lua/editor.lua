49a3b562d0ae0da01205dad7edaf882012dbcc74
Quentin Rasmont <qrasmont@gmail.com>
2021-09-04 13:54:36 +0200

    nvim: Enable ale fixer for rust

--- nvim/lua/editor.lua
+++ nvim/lua/editor.lua
@@ -1,6 +1,6 @@
 -- Ale linters fixers
 vim.g.ale_linters = {python = {'flake8'}}
-vim.g.ale_fixers = {python = {'black', 'isort'}}
+vim.g.ale_fixers = {python = {'black', 'isort'}, rust = {'rustfmt'}}
 vim.g.ale_fix_on_save = 1
 
 -- Support pytthon
