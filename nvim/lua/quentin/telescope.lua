e00c1bb7423f43168b06fa3d73d2d99b6464c62b
Quentin Rasmont <qrasmont@gmail.com>
2021-09-04 13:56:30 +0200

    nvim: Improve Telescope config

--- nvim/lua/quentin/telescope.lua
+++ nvim/lua/quentin/telescope.lua
@@ -1,8 +1,36 @@
-local M = {}
+local M = {} -- Local table
+
+    -- Search files in my nvim config
     M.search_nvim = function()
         require('telescope.builtin').find_files({
             prompt_title = 'Nvim config',
             cwd = '~/dev/personal/dot_config/nvim/',
         })
     end
-return M
+
+    -- Search selected text in visual mode
+    M.grep_selection = function()
+        -- Get the selection trought the 'v' register
+        vim.cmd('noau normal! "vy"')
+        local string = vim.fn.getreg('v')
+
+        require('telescope.builtin').grep_string({
+            prompt_title = 'Search selection',
+            search = string,
+        })
+    end
+
+    -- Customise builtin file browser
+    M.file_browser = function()
+        local layout_conf = {
+            prompt_position = 'top'
+        }
+
+        require('telescope.builtin').file_browser({
+            previewer = false,
+            layout_config = layout_conf,
+        })
+    end
+
+
+return M -- Return the table == exports the functions
--- nvim/lua/telescope.lua
+++ nvim/lua/telescope.lua
@@ -1,6 +1,10 @@
 Map('n', '<leader>ff', '<cmd>lua require(\'telescope.builtin\').find_files()<cr>', {})
 Map('n', '<leader>fg', '<cmd>lua require(\'telescope.builtin\').live_grep()<cr>', {})
-Map('n', '<leader>bb', '<cmd>lua require(\'telescope.builtin\').buffers()<cr>', {})
+Map('n', '<leader>fb', '<cmd>lua require(\'telescope.builtin\').buffers()<cr>', {})
 Map('n', '<leader>fh', '<cmd>lua require(\'telescope.builtin\').help_tags()<cr>', {})
+Map('n', '<leader>fs', '<cmd>lua require(\'telescope.builtin\').grep_string()<cr>', {})
 
+Map('n', '<leader>fd', '<cmd>lua require(\'quentin.telescope\').file_browser()<cr>', {})
 Map('n', '<leader>fn', '<cmd>lua require(\'quentin.telescope\').search_nvim()<cr>', {})
+Map('v', '<leader>fs', '<cmd>lua require(\'quentin.telescope\').grep_selection()<cr>', {})
+
