70bdfa4e764223afbfdcaa295e7ade22aa31d7c8
Quentin Rasmont <qrasmont@gmail.com>
2021-09-07 20:13:48 +0200

    nvim: Remove python ale fixer

--- nvim/lua/editor.lua
+++ nvim/lua/editor.lua
@@ -1,7 +1,7 @@
 -- Ale linters fixers
-vim.g.ale_linters = {python = {'flake8'}}
-vim.g.ale_fixers = {python = {'black', 'isort'}, rust = {'rustfmt'}}
-vim.g.ale_fix_on_save = 1
+-- vim.g.ale_linters = {python = {'flake8'}}
+-- vim.g.ale_fixers = {python = {'black', 'isort'}, rust = {'rustfmt'}}
+-- vim.g.ale_fix_on_save = 1
 
 -- Support pytthon
 vim.g.python3_host_prog = '/usr/bin/python3'
