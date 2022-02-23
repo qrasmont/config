455485bec2fb74980ea52b21390771ff97fe6a38
Quentin Rasmont <qrasmont@gmail.com>
2022-02-23 16:59:31 +0100

    nvim: Fix telescope nvim config path

--- nvim/lua/quentin/telescope.lua
+++ nvim/lua/quentin/telescope.lua
@@ -4,7 +4,7 @@ local M = {} -- Local table
     M.search_nvim = function()
         require('telescope.builtin').find_files({
             prompt_title = 'Nvim config',
-            cwd = '~/dev/personal/dot_config/nvim/',
+            cwd = '~/dev/personal/config/nvim/',
         })
     end
 
