191ac0ecf91bd85fa5cb1eb62c5b1633c2eb722d
Quentin Rasmont <qrasmont@gmail.com>
2022-03-11 22:02:15 +0100

    nvim: Add function to print the content of lua tables

--- nvim/lua/config-utils.lua
+++ nvim/lua/config-utils.lua
@@ -6,3 +6,8 @@ function Map(mode, lhs, rhs, opts)
   if opts then options = vim.tbl_extend('force', options, opts) end
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
+
+-- Print the content of lua tables
+function PRINT(stuff)
+  print(vim.inspect(stuff))
+end
