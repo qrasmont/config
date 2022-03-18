62f1799b71ae723a78de063cc90a1a9cba595d92
Quentin Rasmont <qrasmont@gmail.com>
2022-03-18 17:48:49 +0100

    nvim: Add projectile config

--- nvim/lua/projectile-config.lua
+++ nvim/lua/projectile-config.lua
@@ -1,2 +1,33 @@
+require('projectile').setup{
+    output_behavior = 'notify',
+    notifier = {
+        wait = {
+            wait_text = 'Projectile',
+            rate = 1000,
+        },
+        done = {
+            success_symbol = '✔',
+            success_text = 'Success',
+            fail_symbol = '✖',
+            fail_text = 'Fail',
+            delay = 3000,
+        },
+        loader = {
+            "🌑 ",
+            "🌒 ",
+            "🌓 ",
+            "🌔 ",
+            "🌕 ",
+            "🌖 ",
+            "🌗 ",
+            "🌘 ",
+        }
+    }
+}
+
+vim.cmd([[highlight NotifierText guifg=#fe8019]])
+vim.cmd([[highlight NotifierSuccess guifg=#b8bb26]])
+vim.cmd([[highlight NotifierFail guifg=#fb4934]])
+
 Map('n', '<leader>pp', '<cmd>lua require(\'projectile\').run()<cr>', {})
 Map('n', '<leader>po', '<cmd>lua require(\'projectile\').toggle_output()<cr>', {})
